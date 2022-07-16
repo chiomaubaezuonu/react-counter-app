7d2f67d26c4f5dae586ee8fd8f2121859f2ae490
Dancyangelo102 <chiomaubaezuonu102@gmail.com>
2022-07-16 06:32:04 +0100

    first commit

---
+++ src/components/Age.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+
+export default function Age(){ 
+    const [age, setAge] = React.useState(0)
+    function increase(){
+        setAge(prevState => prevState - 1)
+    }
+
+    function decrease(){
+        setAge(prevState => prevState + 1)
+    }
+
+    const yourAge = new Date();
+    const dob = yourAge.getFullYear() - age;
+
+    return(
+        <div className="container"> 
+          <div className="age-div">
+            <p>You are {age} years old</p>
+            <p>You were born in year {dob}</p>
+          </div>
+          <div className="counter-div"> 
+            <button className="add" onClick={increase}>-</button>
+            <h1 className="number">{age}</h1>
+            <button className="subtract" onClick={decrease}>+</button>
+        </div>   
+         </div>
+    )
+}
---
+++ src/components/Counter.js
@@ -0,0 +1,13 @@
+// import React from "react";
+// import Age from "./components/Age";
+
+
+
+// export default function Counter(){
+//     return(
+//         <div>
+//             <Age />
+//         </div>
+
+//     )
+// }
